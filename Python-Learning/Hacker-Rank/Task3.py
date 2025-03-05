4a6cc82a28ad1c1da884315a48993bd3f521af25
Harendra <harendrabarot19@gmail.com>
2025-03-05 16:42:49 +0530

    Added some mathematicals praticals implementation on HackerRank

---
+++ Python-Learning/Hacker-Rank/Task3.py
@@ -0,0 +1,13 @@
+##The provided code stub reads two integers, a and b, from STDIN.
+## Add logic to print two lines. The first line should contain the result of integer division, a // b . 
+# The second line should contain the result of float division,  a / b .
+
+# Read input values
+a = int(input())
+b = int(input())
+
+# Integer division (floor division)
+print(a // b)
+
+# Float division
+print(a / b)

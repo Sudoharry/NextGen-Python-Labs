7ecec557e85df442fd9b41bed0d740c4a0c18921
Harendra <harendrabarot19@gmail.com>
2025-03-11 17:16:43 +0530

    Added the task and programmes for tracking

---
+++ Python-Learning/Practice-Session/Task10.py
@@ -0,0 +1,12 @@
+
+# # Explicit is better than implicit
+# import math
+# print(math.sqrt(36))
+
+
+def calculate_area(length,width):
+    """Return the area of the rectangle"""
+    return length * width
+
+cal_area= calculate_area(5,5)   
+print(cal_area)
---
+++ Python-Learning/Practice-Session/Task11.py
@@ -0,0 +1,7 @@
+movies = []
+
+movies.append(input("Enter the first movie:"))
+movies.append(input("Enter the second movie:"))
+movies.append(input("Enter the third movie:"))
+
+print("Your 3 favourites movies:", movies)
